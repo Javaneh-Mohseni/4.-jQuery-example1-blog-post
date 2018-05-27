6018a89e0a0536313ee73a5842e0fa09fe8c5670
Javaneh-Mohseni <39635919+Javaneh-Mohseni@users.noreply.github.com>
2018-05-26 23:44:44 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// $('#flashMessage').hide();
+// $('#flashMessage').fadeIn(1000);
+// $('#flashMessage').delay(3000);
+// $('#flashMessage').slideUp();
+//
+//
+// $('#flashMessage').hide().fadeIn(1000).delay(3000).slideUp();
+
+$('#flashMessage')
+  .hide()
+  .fadeIn(1000)
+  .delay(3000)
+  .slideUp();
